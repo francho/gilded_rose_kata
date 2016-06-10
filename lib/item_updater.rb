0fb9fafd84dae0af13d01c614941bf7282615261
Francho Joven <lab@francho.org>
2016-06-10 23:08:25 +0200

    Group backstage? conditions

--- lib/item_updater.rb
+++ lib/item_updater.rb
@@ -5,23 +5,27 @@ class ItemUpdater
   end
 
   def self.update_quality(item)
-    unless aged_brie?(item) || backstage?(item)
-      decrease_quality item unless sulfuras?(item)
-    else
+    if backstage? item
       increase_quality item
-      if backstage?(item)
-        increase_quality item if item.sell_in < 10
-        increase_quality item if item.sell_in < 5
-      end
-    end
+      increase_quality item if item.sell_in < 10
+      increase_quality item if item.sell_in < 5
+      item.quality = 0 if item.sell_in < 0
+    else
 
-    if item.sell_in < 0
-      unless aged_brie? item
-        item.quality = 0 if backstage?(item)
+      unless aged_brie?(item)
         decrease_quality item unless sulfuras?(item)
       else
         increase_quality item
       end
+
+      if item.sell_in < 0
+        unless aged_brie? item
+          decrease_quality item unless sulfuras?(item)
+        else
+          increase_quality item
+        end
+      end
+
     end
   end
 
